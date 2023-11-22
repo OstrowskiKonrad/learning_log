68a4820cb50ba853a6b865cd4239db2424035d4d
Konrad Ostrowski <konradostrrowski@gmail.com>
2023-11-22 11:14:43 +0100

    Troubleshooting

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -133,14 +133,17 @@ if config.is_valid_platform():
     if config.appDir:
         STATIC_ROOT = Path(config.appDir) / 'static'
     if config.projectEntropy:
-        SECRET_KEY = config.credentials('database')
+        SECRET_KEY = SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials('database')
         DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.postgresql',
                 'NAME': db_settings['path'],
-                'USER': db_settings['path'],
-                'PASSWORD': db_settings['host'],
+                'USER': db_settings['username'],
+                'PASSWORD': db_settings['password'],
                 'HOST': db_settings['host'],
                 'PORT': db_settings['port'],
-            },
-        }
+    },
+}
